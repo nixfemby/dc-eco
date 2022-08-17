ff710d12c66d2cf82a52d06bbeef1bc353570292
wxifu <ayowxifu@tutanota.com>
2022-08-17 21:01:25 +0200

    Add Inventory to user profile schema

--- models/profile.js
+++ models/profile.js
@@ -4,6 +4,7 @@ const ProfileSchema = new mongoose.Schema({
     userID: { type: String },
     wallet: { type: Number, default: 1 },
     bank: { type: Number, default: 1 },
+    inventory: { type: Array, default: [], required: true },
     lastUpdated: { type: Date, default: new Date() },
 });
 
